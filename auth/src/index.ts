a42bf42f8c1ffcf656c823b5bca391a4a9a31d77
Gueka <marcojcapo@gmail.com>
2023-04-05 10:57:58 -0400

    added start message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    console.log('Starting app.');
+    console.log('Starting app...');
     // Check mandatory environment variables
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined');
