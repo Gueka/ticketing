5ce1615f4b73fe49e53b4083d3f11ba313accc45
Gueka <marcojcapo@gmail.com>
2023-04-13 07:49:45 -0400

    forcing deployment workflow

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,7 +2,7 @@
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 import { natsWrapper } from './nats-wrapper';
 const start = async () => {
-    console.log('Starting app.');
+    console.log('Starting app!');
     // Check mandatory environment variables
     if(!process.env.NATS_CLUSTER_ID || !process.env.NATS_CLIENT_ID || !process.env.NATS_URL){
         throw new Error('NATS env variables must be defined');
